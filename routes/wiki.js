e40fc3df21851477aa88471c5dc2908ff966a158
Beno Suriano <bjsuriano@gmail.com>
2018-03-09 15:34:05 -0600

    Attempted using hooks.

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,17 +1,44 @@
 const router = require('express').Router()
+const { Page } = require("../models");
 const addPage = require('../views/addPage')
+//replace snippet
+const replace = (str) => {
+  return str.replace(/ /g, "_");
+}
 
 router.get('/', (req, res) => {
     res.send('go to GET /wiki')
 })
 
-router.post('/', (req, res) => {
-    res.send('go to POST /wiki')
-})
+router.post("/", async (req, res, next) => {
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.textArea
+
+    Page.beforeValidate((user) => {
+        user.slug = replace(user.title);
+    });
+  });
+
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
 
 router.get('/add', (req, res) => {
     res.send(addPage())
 })
-
 
 module.exports = router
--- views/addPage.js
+++ views/addPage.js
@@ -1,15 +1,24 @@
 const html = require("html-template-tag");
 const layout = require("./layout");
 
-module.exports = () => layout(html`
+module.exports = () =>
+  layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-    
+
+    <div>
+      <label for="authorName" class="col-sm-2 control-label">Author</label>
+      <div class="col-sm-10">
+        <input id="authorName" name="authorName" type="text" class="form-control"/>
+    </div>
+
+    <div>
+      <label for="authorEmail" class="col-sm-2 control-label">Author Email</label>
+      <div class="col-sm-10">
+        <input id="authorEmail" name="authorEmail" type="text" class="form-control"/>
+    </div>
+
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
@@ -17,13 +26,19 @@ module.exports = () => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <div>
+      <label for="textArea" class="col-sm-2 control-label">Text</label>
+      <div class="col-sm-10">
+        <input id="textArea" name="textArea" type="text" class="form-control"/>
+    </div>
 
+    <div>
+      <label for="pageStatus" class="col-sm-2 control-label">Page Status</label>
+      <div class="col-sm-10">
+        <input id="pageStatus" name="pageStatus" type="text" class="form-control"/></div>
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
